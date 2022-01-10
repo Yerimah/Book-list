c42ceea804620afd64952b5f1bf2ba537c518c6c
Yerimah <danielyerimah@yahoo.com>
2022-01-10 14:23:56 +0100

    add markup, app.js and list.css files. Install linters

---
+++ app.js
@@ -0,0 +1,4 @@
+const bookTitle = document.querySelector('#booktitle');
+const bookAuthor = document.querySelector('#bookauthor');
+const addBtn = document.querySelector('.addbtn');
+const bookList = document.querySelector('.booklist');
